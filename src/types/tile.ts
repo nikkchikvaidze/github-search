6b68b0c860736c3bc959605f0087c0901f45d401
nikkchikvaidze <nikkchikvaidze@gmail.com>
2025-06-26 01:23:21 +0400

    fixed typo

--- src/types/tile.ts
+++ src/types/tile.ts
@@ -1,8 +1,8 @@
-export type Tile {
-    icon: string;
-    title?: string;
-    description: string | number;
-    isDescriptionEmail?: boolean;
-    isDescriptionLink?: boolean;
-    id?: string;
-}
+export type Tile = {
+  icon: string;
+  title?: string;
+  description: string | number;
+  isDescriptionEmail?: boolean;
+  isDescriptionLink?: boolean;
+  id?: string;
+};
